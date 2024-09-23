684661d6413533a238f6d4cfd2d2609a5375d20d
Leonardo Lopez <96030802+lejolo20@users.noreply.github.com>
2024-09-23 15:33:54 -0400

    feat: example done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import { withUt } from "uploadthing/tw";
+
+export default withUt({
+  // Your existing Tailwind config
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+});
